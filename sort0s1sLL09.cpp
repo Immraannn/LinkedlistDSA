d062841a720c0e4d9469e6eb5e7e38406ab4b5f4
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-25 01:02:48 +0530

    Update sort0s1sLL09.cpp

--- sort0s1sLL09.cpp
+++ sort0s1sLL09.cpp
@@ -1,29 +1,22 @@
-
 Node* sortZeroOne(Node* head) {
-
     // Dummy head for list containing 0s
     Node* zeroHead = new Node(-1);
-
     // Dummy head for list containing 1s
     Node* oneHead = new Node(-1);
-
     // Tail pointer for 0 list
     Node* zeroTail = zeroHead;
-
     // Tail pointer for 1 list
     Node* oneTail = oneHead;
-
     // Pointer to traverse original list
     Node* curr = head;
-
     // Traverse original linked list
     while(curr != NULL) {
 
-        if(curr->data == 0) {              // If current node contains 0
-            zeroTail->next = curr;         // Attach node to zero list
-            zeroTail = curr;               // Move zeroTail forward
+        if(curr->data == 0) {              
+            zeroTail->next = curr;        
+            zeroTail = curr;               
         }
-        else {                             // If current node contains 1
+        else {                             
             oneTail->next = curr;          // Attach node to one list
             oneTail = curr;                // Move oneTail forward
         }
